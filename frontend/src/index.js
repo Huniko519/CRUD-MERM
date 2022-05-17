fbfc48528456767a79e0b514be9b52fc96d820c7
Astro2020-lovely <Astro_Ninja@eclipso.eu>
2022-05-17 08:46:25 +0600

    Fist commit

---
+++ frontend/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, Redirect, Route } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+
+import Login from "./Login";
+import Register from "./Register";
+import Admin from "./admin";
+import "./Login.css";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/register" component={Register} />
+      <Route path="/admin" component={Admin} />
+      {/* <Route component={NotFound}/> */}
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
